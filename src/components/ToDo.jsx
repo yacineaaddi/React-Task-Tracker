60e64a7f48781051fa8af0c49f061f7a1a8f52f3
yacine <yacineaaddi@gmail.com>
2025-04-12 19:44:40 +0100

    Deleting Tasks

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import EditTask from "./EditTask";
 
 const ToDo = ({ task, index, tasklist, SetTaskList }) => {
+  const handleDelete = (itemID) => {
+    let removeIndex = tasklist.indexOf(task);
+    tasklist.splice(removeIndex, 1);
+    SetTaskList((currentTasks) =>
+      currentTasks.filter((todo) => todo.id !== itemID)
+    );
+  };
   return (
     <>
       <div className="flex flex-col items-start justify-start bg-white my-4 ml-6 py-4 px-6 w-3/4 max-w-lg">
@@ -17,7 +24,10 @@ const ToDo = ({ task, index, tasklist, SetTaskList }) => {
 
         <p className="text-lg py-2">{task.taskDescription}</p>
         <div className="w-full flex justify-center">
-          <button className="bg-red-500 text-white text-sm uppercase font-senibold py-1.5 px-3 mt-6 mb-1 rounded-lg">
+          <button
+            className="bg-red-500 text-white text-sm uppercase font-senibold py-1.5 px-3 mt-6 mb-1 rounded-lg"
+            onClick={handleDelete}
+          >
             Delete
           </button>
         </div>
